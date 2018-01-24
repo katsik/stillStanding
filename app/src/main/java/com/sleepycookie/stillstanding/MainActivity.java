624be042f77da103ad5b4b78b8db7ce8ed9ad262
George Katsikopoulos <catchiecop@gmail.com>
2018-01-24 20:06:27 +0200

    added new TODOs

--- app/src/main/java/com/sleepycookie/stillstanding/MainActivity.java
+++ app/src/main/java/com/sleepycookie/stillstanding/MainActivity.java
@@ -206,7 +206,7 @@ public class MainActivity extends AppCompatActivity {
                         //TODO check if it does that consistently
                         List<String> allNumbers = new ArrayList<String>();
                         int phoneIdx = 0;
-
+                        //TODO check why some contacts show no numbers while they have.
                         while (phoneCur.moveToNext()) {
                             no = phoneCur.getString(phoneCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
                             int type = phoneCur.getInt(phoneCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.TYPE));
@@ -308,6 +308,7 @@ public class MainActivity extends AppCompatActivity {
 
     public Uri getPhoto(long contactId) {
         Uri contactUri = ContentUris.withAppendedId(ContactsContract.Contacts.CONTENT_URI, contactId);
+        //TODO get high quality image if available
         Uri photoUri = Uri.withAppendedPath(contactUri, ContactsContract.Contacts.Photo.CONTENT_DIRECTORY);
         Cursor cursor = getContentResolver().query(photoUri,
                 new String[] {ContactsContract.Contacts.Photo.PHOTO}, null, null, null);
