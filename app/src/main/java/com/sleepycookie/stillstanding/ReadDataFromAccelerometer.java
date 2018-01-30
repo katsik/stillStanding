2e65c30f23e2ca3bd734bd6ed58239e3500ef5c8
geotsam <geotsam@gmail.com>
2018-01-30 11:36:45 +0200

    Tweaked the UI in the readData activity. Added some code for the siren.

--- app/src/main/java/com/sleepycookie/stillstanding/ReadDataFromAccelerometer.java
+++ app/src/main/java/com/sleepycookie/stillstanding/ReadDataFromAccelerometer.java
@@ -9,6 +9,8 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -59,11 +61,19 @@ public class ReadDataFromAccelerometer extends AppCompatActivity implements Sens
     private PendingIntent pendingIntent;
 
     public Button quitButton;
+    public Button triggerButton;
     public Toast mToast;
 
     private Context mContext;
 
     private Long timeOfFall;
+
+
+    /** Handles playback of all the sound files */
+    private MediaPlayer mMediaPlayer;
+
+    /** Handles audio focus when playing a sound file */
+    private AudioManager mAudioManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -82,6 +92,10 @@ public class ReadDataFromAccelerometer extends AppCompatActivity implements Sens
 
         // Enable the Up button
         ab.setDisplayHomeAsUpEnabled(true);
+
+        triggerButton = (Button) findViewById(R.id.btn_trigger);
+        initTriggerFunctionality();
+        mAudioManager = (AudioManager) this.getSystemService(Context.AUDIO_SERVICE);
 
         quitButton = findViewById(R.id.btn_quit);
         initQuitFunctionality();
@@ -248,6 +262,9 @@ public class ReadDataFromAccelerometer extends AppCompatActivity implements Sens
      * either we had a FP fall detection or the user fell and stood up again so no need to trigger an
      * emergency event.
      */
+
+    //TODO cleanup code
+    //TODO optimize code
     public void checkPosture(){
         //check from the state array to see if the "fell" state became "still"
         //if so the user fell and is laid down
@@ -392,6 +409,18 @@ public class ReadDataFromAccelerometer extends AppCompatActivity implements Sens
         });
     }
 
+    /**
+     *Method used to initialize functionality of Trigger button
+     **/
+    public void initTriggerFunctionality(){
+        triggerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                triggerEmergency();
+            }
+        });
+    }
+
     private Long getTimeOfFall(){
         return timeOfFall;
     }
@@ -404,4 +433,93 @@ public class ReadDataFromAccelerometer extends AppCompatActivity implements Sens
     public static void toastingBoom(String msg){
         Log.d("Activity Detected",msg);
     }
+
+    public void siren(){
+        // Release the media player if it currently exists because we are about to
+        // play a different sound file
+        releaseMediaPlayer();
+
+        // Request audio focus so in order to play the audio file. The app needs to play a
+        // short audio file, so we will request audio focus with a short amount of time
+        // with AUDIOFOCUS_GAIN_TRANSIENT.
+        int result = mAudioManager.requestAudioFocus(mOnAudioFocusChangeListener,
+                AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+
+        if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+            // We have audio focus now.
+
+            // Create and setup the {@link MediaPlayer} for the audio resource associated
+            // with the current word
+            mMediaPlayer = MediaPlayer.create(this, R.raw.alarm);
+
+            // Start the audio file
+            mMediaPlayer.start();
+
+            // Setup a listener on the media player, so that we can stop and release the
+            // media player once the sound has finished playing.
+            mMediaPlayer.setOnCompletionListener(mCompletionListener);
+        }
+    }
+
+    /**
+     * Clean up the media player by releasing its resources.
+     */
+    private void releaseMediaPlayer() {
+        // If the media player is not null, then it may be currently playing a sound.
+        if (mMediaPlayer != null) {
+            // Regardless of the current state of the media player, release its resources
+            // because we no longer need it.
+            mMediaPlayer.release();
+
+            // Set the media player back to null. For our code, we've decided that
+            // setting the media player to null is an easy way to tell that the media player
+            // is not configured to play an audio file at the moment.
+            mMediaPlayer = null;
+
+            // Regardless of whether or not we were granted audio focus, abandon it. This also
+            // unregisters the AudioFocusChangeListener so we don't get anymore callbacks.
+            mAudioManager.abandonAudioFocus(mOnAudioFocusChangeListener);
+        }
+    }
+
+    /**
+     * This listener gets triggered whenever the audio focus changes
+     * (i.e., we gain or lose audio focus because of another app or device).
+     */
+    private AudioManager.OnAudioFocusChangeListener mOnAudioFocusChangeListener = new AudioManager.OnAudioFocusChangeListener() {
+        @Override
+        public void onAudioFocusChange(int focusChange) {
+            if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT ||
+                    focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
+                // The AUDIOFOCUS_LOSS_TRANSIENT case means that we've lost audio focus for a
+                // short amount of time. The AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK case means that
+                // our app is allowed to continue playing sound but at a lower volume. We'll treat
+                // both cases the same way because our app is playing short sound files.
+
+                // Pause playback and reset player to the start of the file. That way, we can
+                // play the word from the beginning when we resume playback.
+                mMediaPlayer.pause();
+                mMediaPlayer.seekTo(0);
+            } else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
+                // The AUDIOFOCUS_GAIN case means we have regained focus and can resume playback.
+                mMediaPlayer.start();
+            } else if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
+                // The AUDIOFOCUS_LOSS case means we've lost audio focus and
+                // Stop playback and clean up resources
+                releaseMediaPlayer();
+            }
+        }
+    };
+
+    /**
+     * This listener gets triggered when the {@link MediaPlayer} has completed
+     * playing the audio file.
+     */
+    private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mediaPlayer) {
+            // Now that the sound file has finished playing, release the media player resources.
+            releaseMediaPlayer();
+        }
+    };
 }
